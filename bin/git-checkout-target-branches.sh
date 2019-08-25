aaf4909b0d71721ad90c78587995f31b37d4e2c3
Shigeru Hagiwara <hgwrsgr@gmail.com>
2019-08-25 09:32:37 +0900

    *** empty log message ***

--- bin/git-checkout-target-branches.sh
+++ bin/git-checkout-target-branches.sh
@@ -11,7 +11,24 @@ trap 'echo "Ctrl-C captured and exit."; exit 1' INT
 trap 'echo "some error occured at $(pwd) and exit."; exit 8' SIGHUP
 
 if [ $# -ne 4 ] || [ "$1" != "into" ]  || [ "$3" != "from" ]; then
-  echo "Usage: git-checkout-target-branches.sh into milestone/abc from feature/cde" 1>&2
-  exit 1
+    echo "Usage: git-checkout-target-branches.sh into milestone/abc from feature/cde" 1>&2
+    exit 1
 fi
 
+dst_branch="$2"
+src_branch="$4"
+
+if [ ! -d .git ]; then
+    echo "Error: no git repository" 1>&2
+    exit 1
+fi
+
+git checkout master
+git fetch -a
+git pull origin master
+
+git checkout "$dst_branch"
+git pull origin "$dst_branch"
+
+git checkout "$src_branch"
+git pull origin "$src_branch"
