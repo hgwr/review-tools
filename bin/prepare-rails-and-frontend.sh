5044121b1cac856e19a93c53aca638d53d7c849a
Shigeru Hagiwara <hgwrsgr@gmail.com>
2019-08-25 09:41:03 +0900

    add bundle install

---
+++ bin/prepare-rails-and-frontend.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Usage: prepare-rails-and-frontend.sh
+#
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+trap 'echo "Ctrl-C captured and exit."; exit 1' INT
+trap 'echo "some error occured at $(pwd) and exit."; exit 8' SIGHUP
+
+if [ ! -d .git ]; then
+    echo "Error: no git repository" 1>&2
+    exit 1
+fi
+
+if [ -f .ruby-version ]; then
+    rbenv local `cat .ruby-version`
+fi
+bundle install --path vendor/bundle --jobs=4 --retry=3
