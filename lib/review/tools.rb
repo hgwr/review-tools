73b61a53c065a52e601092983de17130f3da1a6f
Shigeru Hagiwara <hgwrsgr@gmail.com>
2019-08-25 09:13:02 +0900

    bundle gem review-tools

---
+++ lib/review/tools.rb
@@ -0,0 +1,8 @@
+require "review/tools/version"
+
+module Review
+  module Tools
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/review/tools/version.rb
@@ -0,0 +1,5 @@
+module Review
+  module Tools
+    VERSION = "0.1.0"
+  end
+end
